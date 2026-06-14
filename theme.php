243ae0cbb89fda8589315761d559dcabe6830ca6
Terry Webbs <91281946+terrywebbs@users.noreply.github.com>
2026-06-14 13:00:58 +0100

    Add files via upload

--- theme.php
+++ theme.php
@@ -24,73 +24,34 @@
       <!-- Header Area Wrapped in an Isolated Flex Grid -->
       <header id="header" class="gini-grid-header">
         
-        <!-- Renamed container to mirror the exact row settings of your social links -->
         <a href="<?= $Wcms->url() ?>" class="gini-logo-link">
-  <div class="gini-logo-row">
-    <div class="gini-header-circle">
-      <!-- Image sourced through WonderCMS dynamic root folder paths -->
-      <img src="<?= $Wcms->asset('images/gem.png') ?>" alt="GiniGifts Gem Logo" class="gem-img">
-    </div>
-  </div>
-</a>
+          <div class="gini-logo-row">
+            <div class="gini-header-circle">
+              <img src="<?= $Wcms->asset('images/gem.png') ?>" alt="GiniGifts Gem Logo" class="gem-img">
+            </div>
+          </div>
+        </a>
         
         <!-- The continuous horizontal cross bar anchor line under the logo -->
         <div class="header-horizontal-line"></div>
         
         <div class="content">
           <main>
-          
             <div id="content-area">
               <!-- Render database core values -->
               <?= $Wcms->page('content') ?>
               
-              <!-- Contact Page Spliced Additions Module -->
-              <?php if ($Wcms->page('url') === 'contact' || $Wcms->currentPage === 'contact'): ?>
-                <div id="gini-social-wrapper">
-                  <div class="slideshow-container">
-                    <img src="<?= $Wcms->asset('images/untitled1.png') ?>" alt="Showcase 1">
-                    <img src="<?= $Wcms->asset('images/untitled2.png') ?>" alt="Showcase 2">
-                    <img src="<?= $Wcms->asset('images/untitled3.png') ?>" alt="Showcase 3">
-                    <img src="<?= $Wcms->asset('images/untitled4.png') ?>" alt="Showcase 4">
-
-                  </div>
-                  <p class="contact-prompt">Please use the options below to get in touch</p>
-                  
-                  <div class="icons-row">
-                    <!-- Shop Link -->
-                    <a href="https://ginigifts.sumup.link/" target="_blank" class="social-link-item">
-                      <div class="icon-circle-frame">
-                        <img src="<?= $Wcms->asset('images/gem.png') ?>" alt="Shop" class="social-icon">
-                      </div>
-                      <span class="icon-label">Shop</span>
-                    </a>
-                    <!-- eBay Link -->
-                    <a href="https://www.ebay.co.uk/usr/ginigifts" target="_blank" class="social-link-item">
-                      <div class="icon-circle-frame">
-                        <img src="<?= $Wcms->asset('images/ebay.png') ?>" alt="eBay" class="social-icon">
-                      </div>
-                      <span class="icon-label">eBay</span>
-                    </a>
-                    <!-- Instagram Link -->
-                    <a href="https://www.instagram.com/ginigifts/?hl=en" target="_blank" class="social-link-item">
-                      <div class="icon-circle-frame">
-                        <img src="<?= $Wcms->asset('images/instagram.png') ?>" alt="Instagram" class="social-icon">
-                      </div>
-                      <span class="icon-label">Instagram</span>
-                    </a>
-                  </div>
-                </div>
-              <?php endif; ?>
-
+              <!-- Retained your original parent element wrapper ID configuration -->
+              <div id="gini-social-wrapper">
+                <?= $Wcms->block('giniSocialSection') ?>
+              </div>
             </div>
           </main>
         </div>
         
         <!-- Modern Structural Architectural Navigation Layout Grid -->
         <nav id="gini-grid-nav">
-          <!-- The continuous horizontal cross bar anchor line -->
           <div class="grid-horizontal-line"></div>
-          
           <ul>
             <?= $Wcms->menu() ?>
           </ul>
